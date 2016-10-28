0a074a0314be896b439535009f55b933c034dc0f
Francesco Bedussi <francesco@francescobedussi.it>
2016-10-28 14:45:29 +0200

    INIT

---
+++ popup.js
@@ -0,0 +1,28 @@
+function addUrlParameter(tab, param, value) {
+    var query = `${param}=${value}`;
+    var pattern = `(\\?|&)(${param}=[^&]*)(#[^&]*)`;  
+    var re = new RegExp(pattern);
+    var connector = tab.url.match(/\?/)? '&' : '?';
+    var url = tab.url.match(re)?
+        tab.url.replace(re, '$1' + query + '$3') //$1 = connector, $3 = hash
+        : tab.url.replace(/(.*)(#[^&]*)/, '$1' + connector + query + '$2'); //$1 = url, $2 = hash
+    
+    chrome.tabs.update(tab.id, {'url': url});
+}
+
+[].forEach.call(document.querySelectorAll('[name="device"]'), function(el) {
+    el.addEventListener('click', function(e) {
+        var device = e.target.value;
+        
+        chrome.tabs.getSelected(null, function(tab) {
+            var param = isNaN(device)? 'device' :  'mobile';
+            addUrlParameter(tab, param, device);
+        });
+      }, false);
+});
+
+document.querySelector('.scache').addEventListener('click', function() {
+    chrome.tabs.getSelected(null, function(tab) {
+        addUrlParameter(tab, 'scache', Date.now().toString());
+    });
+});
