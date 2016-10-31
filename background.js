12b48d8dd889a6270d570ffc517b30e1025edbab
Francesco Bedussi <francesco@francescobedussi.it>
2016-10-31 18:13:05 +0100

    keyboard shortcuts init

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-//var devices = ['smartphone', 'tablet', 'desktop'];
-//chrome.browserAction.onClicked.addListener(function(tab) {
-//    if (!active) {
-//        active = true;
-//        chrome.tabs.executeScript(null, {file: "content_script.js"});
-//        chrome.tabs.insertCSS(null, {file: "style.css"});            
-//    } else {
-//        active = false;
-//        chrome.tabs.executeScript(null, {code: "document.body.removeChild(document.querySelector('#fbAdsContainer'));[].forEach.call(document.querySelectorAll('.mark'), function(el){el.classList.remove('mark')});"});
-//    }
+//chrome.commands.onCommand.addListener(function(command) {
+//    console.log('Command:', command);
 //});
